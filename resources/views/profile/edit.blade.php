34bce791581d678696e5712e5b7e28e04e6aa7dd
theomalley <brian@screenplayreaders.com>
2026-06-05 13:55:25 -0700

    portal: move logline card inside isAdminOrEditor||isReader block so it renders for all staff

--- resources/views/profile/edit.blade.php
+++ resources/views/profile/edit.blade.php
@@ -251,7 +251,6 @@
                     </form>
                 </div>
             </div>
-            @endif
 
             <div class="p-4 sm:p-8 bg-white shadow sm:rounded-lg">
                 <div class="max-w-xl">
@@ -275,6 +274,7 @@
                     </form>
                 </div>
             </div>
+            @endif
 
             <div class="p-4 sm:p-8 bg-white shadow sm:rounded-lg">
                 <div class="max-w-xl">
