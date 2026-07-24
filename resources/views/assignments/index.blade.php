376b2636c9181e9a2d025bf4d489c6cb2e5b6176
theomalley <brian@screenplayreaders.com>
2026-07-24 15:37:18 -0700

    adjust assignments screen width for desktop

--- resources/views/assignments/index.blade.php
+++ resources/views/assignments/index.blade.php
@@ -175,7 +175,7 @@
     </script>
 
     <div class="py-6">
-        <div class="max-w-full mx-auto px-4 sm:px-6 lg:px-8">
+        <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
 
             {{-- Flash messages --}}
             @if (session('success'))
